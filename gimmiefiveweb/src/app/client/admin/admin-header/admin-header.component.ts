1a99f84df387bd42421be2b3af820b83e860abe1
kxthhhh <victoriokatherinegail@gmail.com>
2023-11-10 18:00:07 +0800

    newest and logout

--- gimmiefiveweb/src/app/client/admin/admin-header/admin-header.component.ts
+++ gimmiefiveweb/src/app/client/admin/admin-header/admin-header.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-header',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AdminHeaderComponent {
 
+  constructor(
+    private http:HttpClient,
+    private router: Router
+    ) { 
+
+    }
+
+
+
+  logout() {
+    this.http.post('http://localhost:5000/api/logout', null, { withCredentials: true }).subscribe(
+      (response) => {
+        // Handle the successful logout response here
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        // Handle any errors that occur during the logout process
+      }
+    );
+  }
 }
