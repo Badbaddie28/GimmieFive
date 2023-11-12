f578e3a12ad6af4af2add238c3c26e008c39289f
Badbaddie28 <johnchristian.fmagno@gmail.com>
2023-11-12 23:00:14 +0800

    payment and images show-kineme

--- gimmiefiveweb/src/assets/js/uploadphoto.js
+++ gimmiefiveweb/src/assets/js/uploadphoto.js
@@ -1,12 +1,12 @@
-/*  ==========================================
-    SHOW UPLOADED IMAGE
+/* ==========================================
+   SHOW UPLOADED IMAGE
 * ========================================== */
-function readURL(input) {
+function readURL(input, imageResultId) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            $('#imageResult')
+            $('#' + imageResultId)
                 .attr('src', e.target.result);
         };
         reader.readAsDataURL(input.files[0]);
@@ -15,52 +15,31 @@ function readURL(input) {
 
 $(function () {
     $('#upload').on('change', function () {
-        readURL(input);
+        readURL(this, 'imageResult');
+    });
+
+    $('#upload1').on('change', function () {
+        readURL(this, 'imageResult1');
     });
 });
 
-/*  ==========================================
-    SHOW UPLOADED IMAGE NAME
+/* ==========================================
+   SHOW UPLOADED IMAGE NAME
 * ========================================== */
-var input = document.getElementById( 'upload' );
-var infoArea = document.getElementById( 'upload-label' );
+var input = document.getElementById('upload');
+var infoArea = document.getElementById('upload-label');
+input.addEventListener('change', function (event) {
+    showFileName(event, infoArea);
+});
 
-input.addEventListener( 'change', showFileName );
-function showFileName( event ) {
-  var input = event.srcElement;
-  var fileName = input.files[0].name;
-  infoArea.textContent = 'File name: ' + fileName;
+var input1 = document.getElementById('upload1');
+var infoArea1 = document.getElementById('upload-label1');
+input1.addEventListener('change', function (event) {
+    showFileName(event, infoArea1);
+});
+
+function showFileName(event, infoArea) {
+    var input = event.srcElement;
+    var fileName = input.files[0].name;
+    infoArea.textContent = 'File name: ' + fileName;
 }
-
-
-/*  ==========================================
-    MULTIPLE IMAGE
-* ========================================== */
-
-$(function () {
-    $(document).on('click', '.btn-add', function (e) {
-        e.preventDefault();
-
-        var controlForm = $('.controls:first'),
-            currentEntry = $(this).parents('.entry:first'),
-            newEntry = $(currentEntry.clone()).appendTo(controlForm);
-
-        newEntry.find('input').val('');
-        controlForm.find('.entry:not(:last) .btn-add')
-            .removeClass('btn-add').addClass('btn-remove')
-            .removeClass('btn-success').addClass('btn-danger')
-            .html('<span class="fa fa-trash"></span>');
-    }).on('click', '.btn-remove', function (e) {
-        $(this).parents('.entry:first').remove();
-
-        e.preventDefault();
-        return false;
-    });
-});
-
-
-
-// Event handler for the "Edit" button click
-$(document).on('click', '.btn-success', function() {
-    $('#editOrgProfileModal').modal('show');
-});
